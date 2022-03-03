192c9bafa685b36feecf58b9482459350a80468d
onlitstechnologies <onlitstechnologies@gmail.com>
2022-03-03 11:03:50 +0530

    20220303

--- PHP/CommandLine/Database.php
+++ PHP/CommandLine/Database.php
@@ -1,8 +1,15 @@
 <?php
 
-function dtabase_mysqli_procedural()
+function database_mysqli_procedural()
 {
+    require 'Credential.php';
+    $con = mysqli_connect($server, $user, $password, $database);
 
+    if (!$con) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    echo "mysqli_procedural: Connected successfully\n";
 }
 
 function database_mysqli_object_oriented()
@@ -10,16 +17,25 @@ function database_mysqli_object_oriented()
     require 'Credential.php';
     $con = new mysqli($server, $user, $password, $database);
 
-    if($con->connect_error) {
+    if ($con->connect_error) {
         die("Connection failed: " . $con->connect_error);
     }
 
-    echo "Connected successfully\n";
+    echo "mysqli_object_oriented: Connected successfully\n";
 }
 
 function database_pdo()
 {
-    
+    require "Credential.php";
+    try {
+        $con = new PDO("mysql:host=$server; dname=$database", $user, $password);
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "mysqli_pdo: Connected successfully\n";
+    } catch (PDOException $e) {
+        echo "Connection failed: " .$e->getMessage();
+    }
 }
 
+database_mysqli_procedural();
 database_mysqli_object_oriented();
+database_pdo();
