cc76f264b4af45fcff66428dfa29bcb8a3a6c69d
onlitstechnologies <onlitstechnologies@gmail.com>
2022-03-03 10:42:32 +0530

    20220303

---
+++ PHP/CommandLine/Credential_template.php
@@ -0,0 +1,9 @@
+<?php
+$server = "localhost";
+$user = "<your_username";
+$password = "<your_passoword>";
+$database = "hostel_2201";
+
+
+//Rename this file to Credential.php and
+// update relevant details.
--- PHP/CommandLine/Database.php
+++ PHP/CommandLine/Database.php
@@ -1,4 +1,5 @@
 <?php
+
 function dtabase_mysqli_procedural()
 {
 
@@ -6,10 +7,19 @@ function dtabase_mysqli_procedural()
 
 function database_mysqli_object_oriented()
 {
-    
+    require 'Credential.php';
+    $con = new mysqli($server, $user, $password, $database);
+
+    if($con->connect_error) {
+        die("Connection failed: " . $con->connect_error);
+    }
+
+    echo "Connected successfully\n";
 }
 
 function database_pdo()
 {
     
 }
+
+database_mysqli_object_oriented();
