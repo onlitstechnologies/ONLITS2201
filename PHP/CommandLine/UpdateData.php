21ba61d24816749ea8a29b92ff846f9734f6ad78
onlitstechnologies <onlitstechnologies@gmail.com>
2022-03-10 08:03:38 +0530

    20220310

--- PHP/CommandLine/UpdateData.php
+++ PHP/CommandLine/UpdateData.php
@@ -2,16 +2,33 @@
 require "Credential.php";
 $con = new mysqli($server, $user, $password, "testdb_2201");
 
-if($con->connect_error) {
+if ($con->connect_error) {
     echo $con->connect_error . "\n";
     exit(0);
 }
 $employee_id = readline("Enter Employee Id: ");
 $sql = "SELECT * FROM employee WHERE employee_id='$employee_id'";
 $result = $con->query($sql);
-if($result->num_rows>0) {
+if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    echo $row['name'] . "\n";
+    echo "Name: " . $row['name'] . "\n";
+    echo "Contact Number: " . $row['contact_no'] . "\n\n";
+    $name = readline("Enter name: ");
+    $contact_no = readline("Enter contact number: ");
+
+    if (!empty($name)) {
+        $sql = "UPDATE employee SET name='$name' WHERE employee_id='$employee_id'";
+        $con->query($sql);
+    }
+
+    if (!empty($contact_no)) {
+        $sql = "UPDATE employee SET contact_no='$contact_no' WHERE employee_id='$employee_id'";
+        $con->query($sql);
+    }
+
+    echo "Data updated\n";
 } else {
     echo "No records found\n";
 }
+
+$con->close();
