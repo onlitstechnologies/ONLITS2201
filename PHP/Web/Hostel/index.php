e4f577f608307b64f42b698d42063a9e176cefe8
onlitstechnologies <onlitstechnologies@gmail.com>
2022-03-24 09:40:09 +0530

    20220324

--- PHP/Web/Hostel/index.php
+++ PHP/Web/Hostel/index.php
@@ -1,9 +1,9 @@
 <?php
-    require "Utility.php";
-    session_start();
-    if(isset($_SESSION['user_id'])) {
-        utility::js_redirect("Dashboard.php");
-    }
+require "Utility.php";
+session_start();
+if (isset($_SESSION['user_id'])) {
+    utility::js_redirect("Dashboard.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,23 +13,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HMS - Identify Yourself</title>
-    <link rel="stylesheet" href="site.css" type="text/css">
+    <link rel="stylesheet" href="site.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-
     <style>
-        h1 {color: blue;}
+        .container {
+            width: 400px;
+            height: 400px;
+            background-color: #EEEEEE;
+            border: 1px solid #3366FF;
+            padding-top: 100px;
+            margin: 0 auto; /*To center an object*/
+            margin-top: 50px;
+            
+        }
     </style>
-    
 </head>
 
 <body>
-    <div>
-        <h1 style="color: green">HMS 1.0</h1>
-        <h1>Login</h1>
+    <div class="container">
         <form action="Authenticate.php" method="POST">
-            <input type="text" name="user_id" id="user_id" placeholder="User Id">
-            <input type="password" name="password" id="password" placeholder="Password">
-            <button>Login</button>
+            <div class="logo-type-1">
+                <img src="images\Logo.jpg" alt="Logo" width="100">
+            </div>
+            <input type="text" name="user_id" id="user_id" placeholder="User Id" class="form-control-block">
+            <input type="password" name="password" id="password" placeholder="Password" class="form-control-block">
+            <button class="form-control-block submit">Login</button>
             <label name="error_message" id="error_message"></label>
         </form>
     </div>
