25db68dfcc35837042d7beb5c8ecde734d9779a5
onlitstechnologies <onlitstechnologies@gmail.com>
2022-04-09 13:14:17 +0530

    20220409

--- PHP/Web/Hostel/Room.php
+++ PHP/Web/Hostel/Room.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +8,7 @@
     <title>Layout Using Grid</title>
     <link rel="stylesheet" href="site.css">
 </head>
+
 <body>
     <div class="wrapper">
         <header>
@@ -23,10 +25,45 @@
         </nav>
         <main>
             <h1>Room</h1>
+            <div class="room-layout-wrapper">
+                <div class="room" id="101" onclick="clickRoom()"><a>101</a></div>
+                <div class="room"><a href="">102</a></div>
+                <div class="room">103</div>
+                <div class="room">104</div>
+                <div class="room">105</div>
+                <div class="room">106</div>
+                <div class="room">107</div>
+                <div class="room">108</div>
+            </div>
+            <hr>
+            <div class="room-layout-wrapper">
+                <div class="room" id="101" onclick="clickRoom()"><a>201</a></div>
+                <div class="room"><a href="">202</a></div>
+                <div class="room">203</div>
+                <div class="room">204</div>
+                <div class="room">205</div>
+                <div class="room">206</div>
+                <div class="room">207</div>
+                <div class="room">208</div>
+            </div>
         </main>
         <footer>
             <p>&copy;2022, HMS 1.0</p>
         </footer>
     </div>
+    <script>
+        function clickRoom() {
+            let roomno = document.getElementById("101");
+            let roomcolor = window.getComputedStyle(roomno).backgroundImage;
+            if(roomcolor == "linear-gradient(rgb(0, 128, 0), rgb(255, 255, 255))") {
+                roomno.style.backgroundImage = "linear-gradient(rgb(128, 0, 0), rgb(255, 255, 255))";
+            }
+            else
+            {
+                roomno.style.backgroundImage = "linear-gradient(rgb(0, 128, 0), rgb(255, 255, 255))";
+            }
+        }
+    </script>
 </body>
+
 </html>
